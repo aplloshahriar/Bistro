8dbb28f6fd597e0d06d4a8b68f92e1900322e527
aplloshahriar <aaronshahriar.as@gmail.com>
2023-07-19 23:33:00 +0600

    slider problem solved

--- src/Pagers/Home/category/Category.jsx
+++ src/Pagers/Home/category/Category.jsx
@@ -3,12 +3,12 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import { Pagination } from 'swiper/modules';
 
-import slide1 from "../../../assets/home/01.jpg";
-import slide2 from "../../../assets/home/02.jpg";
-import slide3 from "../../../assets/home/03.png";
-import slide4 from "../../../assets/home/04.jpg";
-import slide5 from "../../../assets/home/05.png";
-import slide6 from "../../../assets/home/06.png";
+import slide1 from "../../../assets/home/slide1.jpg";
+import slide2 from "../../../assets/home/slide2.jpg";
+import slide3 from "../../../assets/home/slide3.jpg";
+import slide4 from "../../../assets/home/slide4.jpg";
+import slide5 from "../../../assets/home/slide5.jpg";
+
 
 
 const Category = () => {
@@ -29,7 +29,7 @@ const Category = () => {
         <SwiperSlide> <img src={slide3}alt="" /> </SwiperSlide>
         <SwiperSlide> <img src={slide4}alt="" /> </SwiperSlide>
         <SwiperSlide> <img src={slide5}alt="" /> </SwiperSlide>
-        <SwiperSlide> <img src={slide6}alt="" /> </SwiperSlide>
+       
       
       </Swiper>
     );
