5e960949498cf01de5f55d5b39fef241b9520a23
aplloshahriar <aaronshahriar.as@gmail.com>
2023-07-21 22:08:03 +0600

    from our menu section completed

--- src/Pagers/Home/PopularMenu/PpopularMenu.jsx
+++ src/Pagers/Home/PopularMenu/PpopularMenu.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../../components/Section Title/SectionTitle';
+import MenuItem from '../../Shared/MenuItem/MenuItem';
 
 const PpopularMenu = () => {
+    const [menu, setMenu] = useState([]);
+    useEffect(() => {
+        fetch('menu.json')
+            .then(res => res.json())
+            .then(data => {
+                const popularItems = data.filter(item => item.category === 'popular')
+                setMenu(popularItems)
+            })
+    }, [])
     return (
         <section>
             <SectionTitle
-            heading={'FROM OUR MENU'}
-            subHeading={'Check it out'}
+                heading={'FROM OUR MENU'}
+                subHeading={'Check it out'}
             >
             </SectionTitle>
+
+            <div  className='grid md:grid-cols-2 gap-4'>
+                {
+                    menu.map(item => <MenuItem
+                        key={item._id}
+                        item={item}
+                    ></MenuItem>)
+                }
+            </div>
         </section>
     );
 };
