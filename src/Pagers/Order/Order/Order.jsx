883b07654d2046e31b920477c0fa09f025b7b739
aplloshahriar <aaronshahriar.as@gmail.com>
2023-07-30 01:28:54 +0600

    foodCard complete

--- src/Pagers/Order/Order/Order.jsx
+++ src/Pagers/Order/Order/Order.jsx
@@ -5,9 +5,16 @@ import 'react-tabs/style/react-tabs.css';
 import { useState } from 'react';
 import useMenu from '../../../hooks/useMenu';
 
+import OrderTab from '../OrderTab/OrderTab';
+
 const Order = () => {
     const [tabIndex,setTabIndex]=useState(0);
     const[menu]=useMenu();
+    const desserts = menu.filter(item => item.category === "dessert");
+    const pizzas = menu.filter(item => item.category === "pizza");
+    const salads = menu.filter(item => item.category === "salad");
+    const soups = menu.filter(item => item.category === "soup");
+    const drinks = menu.filter(item => item.category === "drinks");
     return (
         <div>
             <Cover img={orderImg} title={"Order Food"}></Cover>
@@ -20,11 +27,25 @@ const Order = () => {
                     <Tab>Dessert</Tab>
                     <Tab>Drinks</Tab>
                 </TabList>
-                <TabPanel></TabPanel>
-                <TabPanel></TabPanel>
-                <TabPanel></TabPanel>
-                <TabPanel></TabPanel>
-                <TabPanel></TabPanel>
+                <TabPanel>
+                    <OrderTab items={salads}></OrderTab>
+                </TabPanel>
+
+                <TabPanel>
+                <OrderTab items={pizzas}></OrderTab>
+                </TabPanel>
+
+                <TabPanel>
+                <OrderTab items={soups}></OrderTab>
+                </TabPanel>
+
+                <TabPanel>
+                <OrderTab items={desserts}></OrderTab>
+                </TabPanel>
+
+                <TabPanel>
+                <OrderTab items={drinks}></OrderTab>
+                </TabPanel>
             </Tabs>
         </div>
     );
