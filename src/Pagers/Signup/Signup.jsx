9fcb5b58d9435ab591d9b8dbaa8adfc053698afc
aplloshahriar <aaronshahriar.as@gmail.com>
2023-08-07 23:52:53 +0600

    sign up with react hook form

--- src/Pagers/Signup/Signup.jsx
+++ src/Pagers/Signup/Signup.jsx
@@ -1,9 +1,55 @@
-import React from 'react';
+
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+    }
+
+    console.log(watch("example"));
+
     return (
-        <div>
-            <h3>signup</h3>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Sign Up now!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi..</p>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text"  {...register("name", { required: true })} name='name' placeholder="Name" className="input input-bordered" />
+                            {errors.name && <span className='text-red-500'>This field is required</span>}
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email"  {...register("email", { required: true })} name='email' placeholder="email" className="input input-bordered" />
+                            {errors.name && <span className='text-red-500'>This field is required</span>}
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password"  {...register("password",  { required: true, minLength:6, maxLength: 20 })} placeholder="password" className="input input-bordered" />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Sign Up</button>
+                        </div>
+                        <p className=' ms-5 mb-3'>Have An Account? <Link className='text-yellow-500' to='/login'>Login Here </Link></p>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
